c936c7e2915f39c1d04d01a729a2081e2af18cbe
Josh Burton <jjburton@gmail.com>
2012-12-18 17:01:26 -0600

    Logic fix for setting a value on addAttr with enums for a verification use of the function Various function name changes Added cgmBuffer - may prove unecessary if multiMessage attributes are up to the job

--- mayaTools/cgm/core/cgmMeta.py
+++ mayaTools/cgm/core/cgmMeta.py
@@ -181,8 +181,6 @@ class cgmNode(MetaClass):#Should we do this?
 		#object.__setattr__(self, attr, value)	
 
 
-
-	
 	    #Catch for no value flags
 	    DataTypeDefaults={'string': "",
 	                      'int': 0,
@@ -210,7 +208,10 @@ class cgmNode(MetaClass):#Should we do this?
 	    if initialValue is not None and initialCreate:
 		log.info("In mNode.addAttr, setting initialValue of '%s'"%str(initialValue)) 
 		self.__setattr__(attr,initialValue)
-		
+	    
+	    if value and attrType is not 'enum':#Want to be able to really set attr on addAttr call if attr exists
+		self.__setattr__(attr,value)
+
 	    #Easy carry for flag handling - until implemented
 	    #==============  
 	    cgmAttr(self.mNode, attrName = attr, lock=lock,keyable=keyable,hidden = hidden)
@@ -287,7 +288,7 @@ class cgmNode(MetaClass):#Should we do this?
         """       
         if self.isReferenced():
             log.error("'%s' is referenced. Cannot change name"%self.mNode)
-            return
+            return False
 
         if nameChildren:
             NameFactory.doRenameHeir(self.mNode,sceneUnique)
@@ -313,7 +314,7 @@ class cgmNode(MetaClass):#Should we do this?
         except:
             log.warning("'%s.%s' not found"%(self.mNode,attr))
              
-    def copyNameTagsFromObject(self,target,ignore=[False]):
+    def doCopyNameTagsFromObject(self,target,ignore=[False]):
         """
         Get name tags from a target object (connected)
         
@@ -437,7 +438,7 @@ class cgmObject(cgmNode):
     #=========================================================================  
     # Rigging Functions
     #=========================================================================  
-    def copyRotateOrder(self,targetObject):
+    def doCopyRotateOrder(self,targetObject):
         """ 
         Copy the rotate order from a target object to the current instanced maya object.
         """
@@ -453,7 +454,7 @@ class cgmObject(cgmNode):
         buffer = mc.getAttr(targetObject + '.rotateOrder')
         attributes.doSetAttr(self.mNode, 'rotateOrder', buffer) 
 
-    def copyPivot(self,sourceObject):
+    def doCopyPivot(self,sourceObject):
         """ Copy the pivot from a source object to the current instanced maya object. """
         try:
             #If we have an Object Factory instance, link it
@@ -2085,3 +2086,422 @@ class cgmOptionVar(object):
 		self.remove(item)
 		log.warning("'%s' removed from '%s'"%(item,self.name))
 		
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   
+# cgmBuffer - replacement for a multimessage attribute. Stores a list to object
+#=========================================================================
+class cgmBuffer(cgmNode):
+    def __init__(self,node = None, name = None,nodeType = 'network',*args,**kws):
+        """ 
+        Intializes an set factory class handler
+        
+        Keyword arguments:
+        setName(string) -- name for the set
+        
+        """
+        ### input check  
+	super(cgmBuffer, self).__init__(node = node,name = name,nodeType = nodeType) 
+        log.debug("In cgmBuffer.__init__ node is '%s'"%node)
+	
+        self.bufferList = []
+        self.bufferDict = {}	
+	
+	self.updateData()
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    # Properties
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    #value
+    #==============    
+    def getValue(self):
+	return self.bufferDict
+		
+    def setValue(self,value):
+	self.purge()#wipe it to reset it
+	if type(value) is list or type(value) is tuple:
+	    for i in value:
+		self.store(i)
+	else:
+	   self.store(value) 
+	    
+    value = property(getValue, setValue)#get,set
+    
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    # functions
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    def returnNextAvailableCnt(self):
+        """ Get's the next available item number """        
+        userAttrs = self.getUserAttrsAsDict()
+        countList = []
+        for key in userAttrs.keys():
+            if 'item_' in key:
+                splitBuffer = key.split('item_')
+                countList.append(int(splitBuffer[-1]))
+        cnt = 0
+        cntBreak = 0
+        while cnt in countList and cntBreak < 500:
+            cnt+=1
+            cntBreak += 1
+        return cnt
+        
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    # Data
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    def updateData(self,*a,**kw):
+        """ Updates the stored data """
+        self.bufferList = []
+        self.bufferDict = {}
+	
+        for attr in self.getUserAttrs():
+            if 'item_' in attr:
+                a = cgmAttr(self.mNode,attr)
+                data = a.getMessage()
+                if data:
+                    self.bufferList.append(data)
+                    self.bufferDict[attr] = data
+                    
+    def rebuild(self,*a,**kw):
+        """ Rebuilds the buffer data cleanly """ 
+	listCopy = copy.copy(self.bufferList)
+	self.value = listCopy
+	self.updateData()
+                    
+    def store(self,info,*a,**kw):
+        """ 
+        Store information to an object in maya via case specific attribute.
+        
+        Keyword arguments:
+        info(string) -- must be an object in the scene
+        
+        """
+        if not mc.objExists(info):
+	    log.warning("'%s' doesn't exist"%info)
+	    return
+        
+        if info in self.bufferList:
+            log.info("'%s' is already stored on '%s'"%(info,self.mNode))    
+            return
+        
+        cnt = self.returnNextAvailableCnt()    
+        attributes.storeInfo(self.mNode,('item_'+str(cnt)),info,*a,**kw)
+        self.bufferList.append(info)
+        self.bufferDict['item_'+str(cnt)] = info
+        
+    def doStoreSelected(self): 
+        """ Store elected objects """
+        # First look for attributes in the channel box
+        channelBoxCheck = search.returnSelectedAttributesFromChannelBox()
+        if channelBoxCheck:
+            for item in channelBoxCheck:
+                self.store(item)
+            return
+        
+        # Otherwise add the objects themselves
+        toStore = mc.ls(sl=True,flatten=True) or []
+        for item in toStore:
+            try:
+                self.store(item)
+            except:
+                log.warning("Couldn't store '%s'"%(item))     
+        
+    def remove(self,info,*a,**kw):
+        """ Store information to an object in maya via case specific attribute. """
+        if info not in self.bufferList:
+            guiFactory.warning("'%s' isn't already stored '%s'"%(info,self.mNode))    
+            return
+        
+        for key in self.bufferDict.keys():
+            if self.bufferDict.get(key) == info:
+                attributes.doDeleteAttr(self.mNode,key)
+                self.bufferList.remove(info)
+                self.bufferDict.pop(key)
+                
+        log.warning("'%s' removed!"%(info))  
+                
+        self.updateData()
+        
+    def doRemoveSelected(self): 
+        """ Store elected objects """
+        # First look for attributes in the channel box
+        channelBoxCheck = search.returnSelectedAttributesFromChannelBox()
+        if channelBoxCheck:
+            for item in channelBoxCheck:
+                self.remove(item)
+            return
+        
+        # Otherwise add the objects themselves
+        toStore = mc.ls(sl=True,flatten=True) or []
+        for item in toStore:
+            try:
+                self.remove(item)
+            except:
+                log.warning("Couldn't remove '%s'"%(item)) 
+                
+    def purge(self):
+        """ Purge all buffer attributes from an object """
+        
+        userAttrs = mc.listAttr(self.mNode,userDefined = True) or []
+        for attr in userAttrs:
+            if 'item_' in attr:
+                attributes.doDeleteAttr(self.mNode,attr)
+                log.warning("Deleted: '%s.%s'"%(self.mNode,attr))  
+                
+        self.bufferList = []
+        self.bufferDict = {}        
+         
+    def select(self):
+        """ 
+        Select the buffered objects. Need to work out nested searching better
+        only goes through two nexts now
+        """        
+        if self.bufferList:
+            selectList = []
+            # Need to dig down through the items
+            for item in self.bufferList:
+                if search.returnTagInfo(item,'cgmType') == 'objectBuffer':
+                    tmpFactory = cgmBuffer(item)
+                    selectList.extend(tmpFactory.bufferList)
+                    
+                    for item in tmpFactory.bufferList:
+                        if search.returnTagInfo(item,'cgmType') == 'objectBuffer':
+                            subTmpFactory = cgmBuffer(item)   
+                            selectList.extend(subTmpFactory.bufferList)
+                            
+                else:
+                    selectList.append(item)
+                    
+            mc.select(selectList)
+            return
+        
+        log.warning("'%s' has no data"%(self.mNode))  
+        return False
+    
+    def key(self,*a,**kw):
+        """ Select the buffered objects """        
+        if self.bufferList:
+            mc.select(self.bufferList)
+            mc.setKeyframe(*a,**kw)
+            return True
+        
+        log.warning("'%s' has no data"%(self.mNode))  
+        return False
+	    
+class BufferFactory2(object):
+    """ 
+    Buffer Class handler
+    
+    """
+    def __init__(self,bufferName):
+        """ 
+        Intializes an buffer factory class handler
+        
+        Keyword arguments:
+        bufferName(string) -- name for the buffer
+        
+        """
+        ### input check           
+        self.bufferType = ''
+        self.bufferList = []
+        self.bufferDict = {}
+        
+        if mc.objExists(bufferName):
+            self.baseName = search.findRawTagInfo(bufferName,'cgmName')
+            self.storeNameStrings(bufferName)
+            self.updateData()
+            
+        else:
+            self.baseName = bufferName
+            self.create()
+        
+    def create(self):
+        """ Creates a cgm buffer object """        
+        buffer = mc.group(em=True)
+        attributes.storeInfo(buffer,'cgmName',self.baseName)
+        buffer = NameFactory.doNameObject(buffer,True)
+        self.storeNameStrings(buffer)
+        
+    def returnNextAvailableCnt(self):
+        """ Get's the next available item number """        
+        userAttrs = attributes.returnUserAttrsToDict(self.nameLong)
+        countList = []
+        for key in userAttrs.keys():
+            if 'item_' in key:
+                splitBuffer = key.split('item_')
+                countList.append(int(splitBuffer[-1]))
+        cnt = 0
+        cntBreak = 0
+        while cnt in countList and cntBreak < 500:
+            cnt+=1
+            cntBreak += 1
+        return cnt
+        
+        
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    # Data
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    def updateData(self,*a,**kw):
+        """ Updates the stored data """
+        self.bufferList = []
+        self.bufferDict = {}
+        userAttrs = mc.listAttr(self.nameLong,ud=True) or []
+        for attr in userAttrs:
+            if 'item_' in attr:
+                a = AttrFactory(self.nameLong,attr)
+                data = a.getMessage()
+                if data:
+                    self.bufferList.append(data)
+                    self.bufferDict[attr] = data
+                    
+    def rebuild(self,*a,**kw):
+        """ Rebuilds the buffer data cleanly """        
+        if self.bufferDict:
+            #make a copy list            
+            buffer = []
+            for item in self.bufferList:
+                buffer.append(item)
+                
+            #purge our existing stuff
+            self.purge()
+            
+            #add our stuff    
+            for item in buffer:
+                try:
+                    self.store(item)
+                except:
+                    guiFactory.warning("'%s' failed. Probably doesn't exist"%(info))    
+            # Update data        
+            self.updateData()
+                    
+    def store(self,info,*a,**kw):
+        """ 
+        Store information to an object in maya via case specific attribute.
+        
+        Keyword arguments:
+        info(string) -- must be an object in the scene
+        
+        """
+        assert mc.objExists(info) is True, "'%s' doesn't exist"%info
+        
+        if info in self.bufferList:
+            guiFactory.warning("'%s' is already stored on '%s'"%(info,self.nameLong))    
+            return
+        
+        userAttrs = attributes.returnUserAttrsToDict(self.nameLong) or {}
+        countList = []
+        for key in userAttrs.keys():
+            if 'item_' in key:
+                splitBuffer = key.split('item_')
+                countList.append(int(splitBuffer[-1]))
+        cnt = 0
+        cntBreak = 0
+        while cnt in countList and cntBreak < 500:
+            cnt+=1
+            cntBreak += 1
+    
+        attributes.storeInfo(self.nameLong,('item_'+str(cnt)),info,*a,**kw)
+        guiFactory.warning("'%s' stored on '%s'"%(info,self.nameLong))            
+        self.bufferList.append(info)
+        self.bufferDict['item_'+str(cnt)] = info
+        
+    def doStoreSelected(self): 
+        """ Store elected objects """
+        # First look for attributes in the channel box
+        channelBoxCheck = search.returnSelectedAttributesFromChannelBox()
+        if channelBoxCheck:
+            for item in channelBoxCheck:
+                self.store(item)
+            return
+        
+        # Otherwise add the objects themselves
+        toStore = mc.ls(sl=True,flatten=True) or []
+        for item in toStore:
+            try:
+                self.store(item)
+            except:
+                guiFactory.warning("Couldn't store '%s'"%(item))     
+        
+    def remove(self,info,*a,**kw):
+        """ Store information to an object in maya via case specific attribute. """
+        if info not in self.bufferList:
+            guiFactory.warning("'%s' isn't already stored '%s'"%(info,self.nameLong))    
+            return
+        
+        for key in self.bufferDict.keys():
+            if self.bufferDict.get(key) == info:
+                attributes.doDeleteAttr(self.nameLong,key)
+                self.bufferList.remove(info)
+                self.bufferDict.pop(key)
+                
+        guiFactory.warning("'%s' removed!"%(info))  
+                
+        self.updateData()
+        
+    def doRemoveSelected(self): 
+        """ Store elected objects """
+        # First look for attributes in the channel box
+        channelBoxCheck = search.returnSelectedAttributesFromChannelBox()
+        if channelBoxCheck:
+            for item in channelBoxCheck:
+                self.remove(item)
+            return
+        
+        # Otherwise add the objects themselves
+        toStore = mc.ls(sl=True,flatten=True) or []
+        for item in toStore:
+            try:
+                self.remove(item)
+            except:
+                guiFactory.warning("Couldn't remove '%s'"%(item)) 
+                
+    def purge(self):
+        """ Purge all buffer attributes from an object """
+        
+        userAttrs = mc.listAttr(self.nameLong,userDefined = True) or []
+        for attr in userAttrs:
+            if 'item_' in attr:
+                attributes.doDeleteAttr(self.nameLong,attr)
+                guiFactory.warning("Deleted: '%s.%s'"%(self.nameLong,attr))  
+                
+        self.bufferList = []
+        self.bufferDict = {}        
+         
+    def select(self):
+        """ 
+        Select the buffered objects. Need to work out nested searching better
+        only goes through two nexts now
+        """        
+        if self.bufferList:
+            selectList = []
+            # Need to dig down through the items
+            for item in self.bufferList:
+                if search.returnTagInfo(item,'cgmType') == 'objectBuffer':
+                    tmpFactory = BufferFactory(item)
+                    selectList.extend(tmpFactory.bufferList)
+                    
+                    for item in tmpFactory.bufferList:
+                        if search.returnTagInfo(item,'cgmType') == 'objectBuffer':
+                            subTmpFactory = BufferFactory(item)   
+                            selectList.extend(subTmpFactory.bufferList)
+                            
+                else:
+                    selectList.append(item)
+                    
+            mc.select(selectList)
+            return
+        
+        guiFactory.warning("'%s' has no data"%(self.nameShort))  
+        return False
+    
+    def key(self,*a,**kw):
+        """ Select the buffered objects """        
+        if self.bufferList:
+            mc.select(self.bufferList)
+            mc.setKeyframe(*a,**kw)
+            return True
+        
+        guiFactory.warning("'%s' has no data"%(self.nameShort))  
+        return False
+    
+
+
+        
+
+    
