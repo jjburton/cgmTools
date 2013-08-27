1e1c46a9a47b08bd26149986e298628eab4f8f56
Josh Burton <jjburton@gmail.com>
2013-08-27 16:39:27 -0500

    Fix on a bad logic call

--- mayaTools/cgm/core/lib/curve_Utils.py
+++ mayaTools/cgm/core/lib/curve_Utils.py
@@ -67,10 +67,11 @@ def returnSplitCurveList(curve = None,points = 3, markPoints = False,
 	try:
 		mi_crv = cgmMeta.validateObjArg(curve,cgmMeta.cgmObject,mayaType='nurbsCurve',noneValid=False)
 		int_points = cgmValid.valueArg(points,minValue=3,calledFrom=_str_funcName)
+		f_points = float(int_points)
 		int_spans = int(cgmValid.valueArg(points,minValue=5,autoClamp=True,calledFrom=_str_funcName))
 		b_rebuild = cgmValid.boolArg(rebuildForSplit,calledFrom=_str_funcName)
 		b_markPoints = cgmValid.boolArg(markPoints,calledFrom=_str_funcName)
-	
+		
 		#>>> Rebuild curve
 		if b_rebuild:
 			useCurve = mc.rebuildCurve (curve, ch=0, rpo=0, rt=0, end=1, kr=0, kcp=0, kep=1, kt=0, s=int_spans, d=3, tol=0.001)[0]
@@ -86,28 +87,35 @@ def returnSplitCurveList(curve = None,points = 3, markPoints = False,
 	
 		#Figure out our u's
 		l_uValues = [0]
-		f_factor = f_maxU/(points-1)
+		f_factor = f_maxU/(f_points-1)
 		log.info("%s >> f_maxU : %s"%(_str_funcName,f_maxU)) 
 		log.info("%s >> f_factor : %s"%(_str_funcName,f_factor))               
-		for i in range(1,points-1):
+		for i in range(1,int_points-1):
 			l_uValues.append(i*f_factor)
 		l_uValues.append(f_maxU)
 		log.info("%s >> l_uValues : %s"%(_str_funcName,l_uValues))  
 	
 		for u in l_uValues:
-			l_spanUPositions.append(mc.pointPosition("%s.u[%f]"%(useCurve,u)))
+			try:l_spanUPositions.append(mc.pointPosition("%s.u[%f]"%(useCurve,u)))
+			except StandardError,error:raise StandardError,"failed on pointPositioning: %s"%u			
 		log.info("%s >> l_spanUPositions | len: %s | list: %s"%(_str_funcName,len(l_spanUPositions),l_spanUPositions))  
-	
-		if b_markPoints:
-			ml_built = []
-			for i,pos in enumerate(l_spanUPositions):
-				if markPoints == 'locator':
-					buffer =  mc.spaceLocator(n = "%s_u_%s"%(useCurve,l_uValues[i]))[0] 
+		
+		try:
+			if b_markPoints:
+				ml_built = []
+				for i,pos in enumerate(l_spanUPositions):
+					buffer =  mc.spaceLocator(n = "%s_u_%f"%(useCurve,(l_uValues[i])))[0] 
 					ml_built.append( cgmMeta.cgmObject(buffer))
+					log.info("%s >> created : %s | at: %s"%(_str_funcName,ml_built[-1].p_nameShort,pos))              											
 					mc.xform(ml_built[-1].mNode, t = (pos[0],pos[1],pos[2]), ws=True)
-			f_distance = distance.returnAverageDistanceBetweenObjects([o.mNode for o in ml_built]) * .5
-			for o in ml_built:
-				o.scale = [f_distance,f_distance,f_distance]
+				
+				try:f_distance = distance.returnAverageDistanceBetweenObjects([o.mNode for o in ml_built]) * .5
+				except StandardError,error:raise StandardError,"Average distance fail. Objects: %s| error: %s"%([o.mNode for o in ml_built],error)
+				try:
+					for o in ml_built:
+						o.scale = [f_distance,f_distance,f_distance]
+				except StandardError,error:raise StandardError,"Scale fail : %s"%error
+		except StandardError,error:log.error("%s >>> Mark points fail. error : %s"%(_str_funcName,error))
 		if b_rebuild:mc.delete(useCurve)
 		return l_spanUPositions
 	except StandardError,error:
