5e9390b56160ad86ead2a90b7c4960579691f45a
Josh Burton <jjburton@gmail.com>
2013-09-12 10:23:16 -0500

    Moving where some path setups happens for one of our users. Gonna see how we like this.

--- mayaTools/cgm/__init__.py
+++ mayaTools/cgm/__init__.py
@@ -1,7 +1,26 @@
 import os
-from cgm.lib.zoo.zooPy.path import Path
+#from cgm.lib.zoo.zooPy.path import Path
 """ From http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python"""
 
+#Need to setup our cgm paths ===========================================================
+from cgm.lib.zoo.zooPy.path import Path, findFirstInEnv, findInPyPath
+import sys
+
+def setupContributorPaths():
+        thisFile = Path( __file__ )
+        thisPath = thisFile.up()
+
+        mayaSysPaths = sys.path
+
+        for folder in 'mel','images','lib/zoo','lib/zoo/zooMel','lib/zoo/zooPy','lib/zoo/zooPyMaya','lib/bo','lib/ml':
+                bufferFolderPath = thisPath / folder
+                if bufferFolderPath not in mayaSysPaths:
+                        try:
+                                sys.path.append("%s" %bufferFolderPath)
+                        except:
+                                print ('%s Failed to append' %bufferFolderPath)
+setupContributorPaths()
+#=======================================================================================
 
 def returnPyFilesFromFolder():
         import os
