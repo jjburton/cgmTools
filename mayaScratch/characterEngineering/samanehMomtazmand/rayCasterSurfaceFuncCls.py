5eb3f40d7dad66436d8702021c4aecc2a4def29d
Samaneh Momtazmand <samaneh.mtz@gmail.com>
2014-03-29 08:59:55 +0000

    rayCasterSurfaceFuncCls.py edited online with Bitbucket

--- mayaScratch/characterEngineering/samanehMomtazmand/rayCasterSurfaceFuncCls.py
+++ mayaScratch/characterEngineering/samanehMomtazmand/rayCasterSurfaceFuncCls.py
@@ -74,7 +74,6 @@ def rayCasterSurfaceFuncCls(*args, **kws):
                 if len(mc.ls(surface))>1:
                     raise StandardError,"findSurfaceIntersection>>> More than one surface named: %s"%surface
                 
-                self.surfaceShape = mc.listRelatives(surface, s=1)
                 self.centerPoint = mc.xform(surface, q=1, ws=1, t=1)
                 
                 #checking the type 
@@ -88,7 +87,7 @@ def rayCasterSurfaceFuncCls(*args, **kws):
                     self.rayDirection = om.MVector(rayDir[0], rayDir[1], rayDir[2])
                     self.hitPoint = om.MPoint()
                     self.selectionList = om.MSelectionList()
-                    self.selectionList.add(self.surfaceShape)
+                    self.selectionList.add(self.surface)
                     self.surfacePath = om.MDagPath()
                     self.selectionList.getDagPath(0, self.surfacePath)
                     self.surfaceFn = om.MFnNurbsSurface(self.surfacePath)
@@ -170,7 +169,6 @@ def rayCasterSurfaceFuncCls(*args, **kws):
                 if len(mc.ls(surface))>1:
                     raise StandardError,"findSurfaceIntersections>>> More than one surface named: %s"%surface
 
-                self.surfaceShape = mc.listRelatives(surface, s=1)
                 self.centerPoint = mc.xform(surface, q=1, ws=1, t=1)
 
                 #checking the type 
@@ -184,7 +182,7 @@ def rayCasterSurfaceFuncCls(*args, **kws):
                     self.rayDirection = om.MVector(rayDir[0], rayDir[1], rayDir[2])
                     self.hitPoints = om.MPoint()
                     self.selectionList = om.MSelectionList()
-                    self.selectionList.add(surfaceShape)
+                    self.selectionList.add(surface)
                     self.surfacePath = om.MDagPath()
                     self.selectionList.getDagPath(0, self.surfacePath)
                     self.surfaceFn = om.MFnNurbsSurface(self.surfacePath)
