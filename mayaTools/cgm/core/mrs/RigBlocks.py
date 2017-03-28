27fd996d9fce335b6bdb34883a47456010359287
J Burton <jjburton@gmail.com>
2017-03-27 23:56:23 -0500

    CURVES - few tweaks on some shapes RIGBLOCKS - Working through stuff BOX - initial put

---
+++ mayaTools/cgm/core/mrs/RigBlocks.py
@@ -0,0 +1,302 @@
+"""
+------------------------------------------
+RigBlocks: cgm.core.mrs
+Author: Josh Burton
+email: jjburton@cgmonks.com
+
+Website : http://www.cgmonks.com
+------------------------------------------
+
+================================================================
+"""
+import maya.cmds as mc
+
+import random
+import re
+import copy
+import time
+
+# From Red9 =============================================================
+from Red9.core import Red9_Meta as r9Meta
+from Red9.core import Red9_AnimationUtils as r9Anim
+
+#========================================================================
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+#========================================================================
+
+# From cgm ==============================================================
+from cgm.core import cgm_General as cgmGEN
+from cgm.core import cgm_Meta as cgmMeta
+
+from cgm.core.lib import curve_Utils as CURVES
+from cgm.core.lib import attribute_utils as ATTRS
+from cgm.core.lib import position_utils as POS
+from cgm.core.lib import math_utils as MATH
+from cgm.core.lib import distance_utils as DIST
+from cgm.core.lib import snap_utils as SNAP
+from cgm.core.lib import rigging_utils as RIGGING
+from cgm.core.rigger.lib import joint_Utils as JOINTS
+
+#from cgm.core.lib import nameTools
+#from cgm.core.rigger import ModuleFactory as mFactory
+#from cgm.core.rigger import PuppetFactory as pFactory
+#from cgm.core.classes import NodeFactory as nodeF
+
+from cgm.core.mrs.blocks import box
+reload(box)
+
+_d_blockTypes = {'box':box}
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Rig Blocks
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+d_attrstoMake = {'version':'string',#Attributes to be initialzed for any module
+                 'buildAs':'string',
+                 'blockType':'string',
+                 #'autoMirror':'bool',
+                 'direction':'none:left:right:center',
+                 'position':'none:front:back:upper:lower:forward',
+                 'moduleTarget':'messageSimple',
+                 'blockMirror':'messageSimple'}
+
+
+class factory(object):
+    _l_controlLinks = []
+    _l_controlmsgLists = []	
+    
+    def __init__(self, root = None, *a,**kws):
+        """
+        Core rig block factory. Runs processes for rig blocks.
+        
+        
+        """
+        _str_func = 'factory._init_'
+        
+        if a:log.debug("|{0}| >> a: {1}".format(_str_func,a))
+        if kws:
+            self._call_kws = kws
+            cgmGEN.log_info_dict(kws,_str_func)
+            #log.debug("|{0}| >> kws: {1}".format(_str_func,kws))
+        
+        self._mi_root = None
+        
+        _verify = kws.get('verify',False)
+        log.debug("|{0}| >> verify: {1}".format(_str_func,_verify))
+        
+        if root is not None:
+            self.rigBlock_set(root)
+    
+    def __repr__(self):
+        try:return "{0}(root: {1})".format(self.__class__, self._mi_root)
+        except:return self
+        
+    def rigBlock_create(self):
+        raise NotImplementedError,"Not yet..."
+    
+    
+    def get_attrCreateDict(self,blockType = None):
+        """
+        """
+        _str_func = 'get_attrCreateDict'
+        
+        _mod = _d_blockTypes.get(blockType,False)
+        if not _mod:
+            log.warning("|{0}| >> No module found for: {1}".format(_str_func,blockType))
+            return False
+                
+        try:d_attrsFromModule = _mod.d_attrsToMake
+        except:d_attrsFromModule = {}
+        
+        try:d_defaultSettings = _mod.d_defaultSettings
+        except:d_defaultSettings = {}
+
+        try:_l_msgLinks = _mod._l_controlLinks
+        except:_l_msgLinks = []
+        
+        _d = copy.copy(d_attrstoMake)
+        for k,v in d_attrsFromModule.iteritems():
+            if k in _d.keys():
+                log.warning("|{0}| >> key: {1} already in to create list of attributes from default. | blockType: {2}".format(_str_func,k,blockType))                
+            else:
+                _d[k] = v
+                
+        if _l_msgLinks:
+            for l in _l_msgLinks:
+                _d[l] = 'messageSimple'
+        
+        cgmGEN.log_info_dict(_d,_str_func + " '{0}' attributes to create".format(blockType))
+        cgmGEN.log_info_dict(d_defaultSettings,_str_func + " '{0}' defaults".format(blockType))
+        
+        self._d_attrsToVerify = _d
+        self._d_attrToVerifyDefaults = d_defaultSettings
+        
+        #for k in _d.keys():
+            #print k
+        
+        return True
+        
+       
+    
+    def rigBlock_verify(self, blockType = None):
+        _str_func = 'rigBlock_verify'
+        
+        if self._mi_root is None:
+            raise ValueError,"No root loaded."
+        
+        _mRoot = self._mi_root
+        
+        if _mRoot.isReferenced():
+            raise ValueError,"Referenced node. Cannot verify"
+        
+        if not self.get_attrCreateDict(blockType):
+            raise ValueError, "|{0}| >> Failed to get attr dict".format(_str_func,blockType)
+        
+        #Need to get the type, the get the attribute lists and data from the module
+        #...attributes
+        
+        _mRoot.verifyAttrDict(self._d_attrsToVerify,keyable = False, hidden = False)
+        _mRoot.addAttr('blockType', value = blockType,lock=True)	
+        
+        
+        for k,v in self._d_attrToVerifyDefaults.iteritems():
+            try:ATTRS.set(_mRoot.mNode,k,v)
+            except Exception,err:
+                log.error("|{0}| >> Failed to set default value. || key: {1} | value: {2} ||| err: {3}".format(_str_func,k,v,err))                
+            
+        return True
+        
+        
+        return False
+        #>>> Block transform ==================                   
+        _mRoot.addAttr('mClass', initialValue='cgmRigBlock',lock=True) 
+        _mRoot.addAttr('cgmType', value = 'rigHelper',lock=True)	
+    
+        if self.kw_name:#If we have a name, store it
+            _mRoot.addAttr('cgmName',self.kw_name,attrType='string',lock=True)
+        elif 'buildAs' in kws.keys():
+            _mRoot.addAttr('cgmName',kws['buildAs'],attrType='string',lock=True)	    
+    
+        #Store tags from init call
+        #==============  
+        for k in self.kw_callNameTags.keys():
+            if self.kw_callNameTags.get(k):
+                #if log.getEffectiveLevel() == 10:log.debug(k + " : " + str(self.kw_callNameTags.get(k)))                
+                self.addAttr(k,value = self.kw_callNameTags.get(k),lock = True)
+                #if log.getEffectiveLevel() == 10:log.debug(str(self.getNameDict()))
+                #if log.getEffectiveLevel() == 10:log.debug(self.__dict__[k])
+    
+    
+        #Attrbute checking
+        #=================
+        self.verifyAttrDict(d_rigBlockAttrs_toMake,keyable = False, hidden = False)
+        #if log.getEffectiveLevel() == 10:log.debug("%s.__verify__ >>> kw_callNameTags: %s"%(self.p_nameShort,self.kw_callNameTags))	    	
+        d_enumToCGMTag = {'cgmDirection':'direction','cgmPosition':'position'}
+        for k in d_enumToCGMTag.keys():
+            if k in self.kw_callNameTags.keys():
+                try:self.__setattr__(d_enumToCGMTag.get(k),self.kw_callNameTags.get(k))
+                except Exception,error: log.error("%s.__verify__ >>> Failed to set key: %s | data: %s | error: %s"%(self.p_nameShort,k,self.kw_callNameTags.get(k),error))
+    
+        self.doName()   
+    
+        return True        
+        
+    
+    def rigBlock_set(self,root=None):
+        _str_func = 'rigBlock_set'
+        log.debug("|{0}| >> root kw: {1}".format(_str_func,root))
+        if root is None:
+            return False
+        
+        self._mi_root = cgmMeta.validateObjArg(root,'cgmObject')
+        log.debug("|{0}| >> mInstance: {1}".format(_str_func,self._mi_root))
+        pass
+    
+    
+def build_skeleton(joints = 1, curve=None):
+    _str_func = 'build_skeleton'
+    
+    _base = 'joint_base_placer'
+    _top = 'joint_top_placer'
+    _axisAim = 'z+'
+    _axisUp = 'y+'
+    _worldUp = 'orient_crv'
+    
+    _axisWorldUp = MATH.get_obj_vector(_worldUp,'y+')
+    
+    
+    #>>Get positions ==================================================================================
+    _p_start = POS.get(_base)
+    _p_top = POS.get(_top)
+    
+    _l_pos = []
+    _radius = 1
+    
+    if joints == 1:
+        _l_pos = [DIST.get_average_position([_p_start,_p_top])]
+    elif joints == 2:
+        _l_pos = [_p_start,_p_top]
+    else:
+        _vec = MATH.get_vector_of_two_points(_p_start, _p_top)
+        _max = DIST.get_distance_between_points(_p_start,_p_top)
+        
+        log.debug("|{0}| >> start: {1} | end: {2} | vector: {3}".format(_str_func,_p_start,_p_top,_vec))   
+        
+        _split = _max/(joints-1)
+        for i in range(joints-1):
+            _p = DIST.get_pos_by_vec_dist(_p_start, _vec, _split * i)
+            _l_pos.append( _p)
+        _l_pos.append(_p_top)
+        _radius = _split/2
+    
+    #>>Create joints =================================================================================
+    _ml_joints = []
+    
+    log.debug("|{0}| >> pos list...".format(_str_func)) 
+    for i,p in enumerate(_l_pos):
+        log.debug("|{0}| >> {1}:{2}".format(_str_func,i,p)) 
+        
+        _mJnt = cgmMeta.cgmObject(mc.joint (p=(p[0],p[1],p[2])))
+        _mJnt.displayLocalAxis = 1
+        _mJnt.radius = _radius
+        
+        _ml_joints.append ( _mJnt )
+        _mJnt.parent = False
+        
+    #>>Orient chain...
+    for i,mJnt in enumerate(_ml_joints[:-1]):
+        if i > 0:
+            mJnt.parent = _ml_joints[i-1]
+            
+        mDup = mJnt.doDuplicate(parentOnly = True)
+        mc.makeIdentity(mDup.mNode, apply = 1, jo = 1)#Freeze
+        
+        SNAP.aim(mDup.mNode,_ml_joints[i+1].mNode,_axisAim,_axisUp,'world')
+        
+        
+        #_rot = mJnt.rotate
+        mJnt.rotate = 0,0,0
+        mJnt.jointOrient = mDup.rotate
+        #mJnt.rotate = 0,0,0
+        mDup.delete()
+        
+    #>>Last joint....        
+    _ml_joints[-1].parent = _ml_joints[-2]
+    _ml_joints[-1].jointOrient = 0,0,0
+    #_ml_joints[-1].rotate = _ml_joints[-2].rotate
+    #_ml_joints[-1].rotateAxis = _ml_joints[-2].rotateAxis
+    
+    #JOINTS.metaFreezeJointOrientation(_ml_joints)
+    
+    #>>Wiring and naming =================================================================================
+    
+
+            
+            
+            
+    
+    
+        
+
---
+++ mayaTools/cgm/core/mrs/blocks/box.py
@@ -0,0 +1,76 @@
+"""
+------------------------------------------
+cgm.core.mrs.blocks.box
+Author: Josh Burton
+email: jjburton@cgmonks.com
+
+Website : http://www.cgmonks.com
+------------------------------------------
+
+================================================================
+"""
+
+# From Python =============================================================
+import copy
+import re
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+# From Maya =============================================================
+import maya.cmds as mc
+
+# From Red9 =============================================================
+from Red9.core import Red9_Meta as r9Meta
+
+# From cgm ==============================================================
+
+#>>>Block data
+__version__ = 'alpha.03232017'
+
+
+d_attrsToMake = {'joints':'int',
+                 'proxyType':'none:castMesh',
+                 'buildBank':'bool'} 
+
+d_defaultSettings = {'version':__version__,'joints':1,
+                     'blockType':__name__.split('.')[-1],
+                     'buildBank':True,'direction':'center',
+                     'proxyType':'castMesh'}
+
+#d_helperSettings = {'iris':{'plug':'irisHelper','check':'buildIris'},
+                    #'pupil':{'plug':'pupilHelper','check':'buildIris'}}
+
+#These lists should be set up per rigblock as a way to get controls from message links
+#auto validate controlLinks as messageSimple attrs
+_l_controlLinks = ['helperBase','helperTop','helperOrient']
+_l_controlmsgLists = ['helperJoints']
+
+
+#>>> Skeleton
+#=========================================================================================================
+__l_jointAttrs__ = ['rigJoints','influenceJoints','fkJoints','ikJoints','blendJoints']   
+__d_preferredAngles__ = {'shoulder':[0,-10,10],'elbow':[0,-10,0]}#In terms of aim up out for orientation relative values, stored left, if right, it will invert
+__d_controlShapes__ = {'shape':['segmentIK','controlsFK','midIK','settings','hand']}
+
+
+def build_joints(root=None,module=None):
+    """
+    Core rig block factory. Runs processes for rig blocks.
+    """ 
+    _str_func = 'build_joints'
+    
+    
+    
+    
+
+
+
+
+
+
+
+
