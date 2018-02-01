e7055a62faf4e275073e77576abd2eb31b1441ad
Josh Burton <jjburton@gmail.com>
2018-02-01 14:51:49 -0600

    cgmUPDATE module intially done.

--- mayaTools/cgmUpdate.py
+++ mayaTools/cgmUpdate.py
@@ -19,7 +19,7 @@ import json
 import pprint
 import os
 import zipfile
-from shutil import move
+from shutil import move,rmtree
 
 import logging
 logging.basicConfig()
@@ -28,7 +28,9 @@ log.setLevel(logging.DEBUG)
 
 _pathMain = 'https://bitbucket.org/jjburton/cgmtools/commits/'
 _pathPull =  "https://bitbucket.org/jjburton/cgmtools/get/"
-_pathMount  = 'https://api.bitbucket.org/2.0/repositories/jjburton/cgmtools/commits/?until='
+#_pathMount  = 'https://api.bitbucket.org/2.0/repositories/jjburton/cgmtools/commits/?until='
+_pathMount  = 'https://api.bitbucket.org/2.0/repositories/jjburton/cgmtools/commits/'
+_defaultBranch = 'MRS'
 _sep = os.sep
 
 global CGM_BUILDS_DAT
@@ -45,14 +47,40 @@ def get_install_path():
     try:
         #First we'll see if we have cgm installed...
         _path = _sep.join(__file__.split(_sep)[:-1])
-        if 'repos' in _path:
-            log.error("Please don't install to your repos: {0}".format(_path))
-            #_path = False
+        for check in ['repos','mayaTools']:
+            if check in _path:
+                log.error("Please don't install to your repos. Found check: '{0}' | path: {1}".format(check,_path))
+                break
+                #_path = False
     except:pass
     
     return _path
+
+_l_to_clean = ['cgm','cgmToolbox.mel','cgmToolbox.py','Red9',]#...'cgmUpdate.py'
+def clean_install_path(path = None):
+    if path == None:
+        path = get_install_path()
+        for check in ['repos','mayaTools']:
+            if check in path:
+                return log.error("Please don't clean your repos. Found check: '{0}' | path: {1}".format(check,path))
     
-def unzip(zFile = 'D://Dropbox//My Documents//maya//2018//scripts//fcb07f5e84eb0db7c97e00d9d164f9a117545e34.zip'):
+    _path = os.path.abspath(path)
+    pprint.pprint(vars())
+    
+    for f in os.listdir(_path):
+        if f in _l_to_clean:
+            _pathFile = _sep.join([_path,f])
+            log.debug("Cleaning: {0} >> {1} ...".format(f,_pathFile))
+            if '.' in f:
+                os.unlink(_pathFile)
+            else:
+                rmtree(_pathFile)
+ 
+    
+#zFile = 'D:\\Dropbox\\My Documents\\maya\\2018\\scripts\\stuff.zip'
+zFile = 'D:\\Dropbox\\My Documents\\maya\\2018\\scripts\\0c122db7cfd4d0269df8be1bf72269c4b86d3870.zip'
+
+def unzip(zFile = zFile, deleteZip = False, cleanFirst = False):
     if not os.path.exists(zFile):
         return log.error("Bad zip path: {0}".format(zFile))
     
@@ -60,6 +88,7 @@ def unzip(zFile = 'D://Dropbox//My Documents//maya//2018//scripts//fcb07f5e84eb0
     _dir = _sep.join(_path.split(_sep)[:-1])
     _match = True
     pprint.pprint(vars())
+    
     
     #while _match:
     #    for c in ["/",_sep]:
@@ -74,43 +103,92 @@ def unzip(zFile = 'D://Dropbox//My Documents//maya//2018//scripts//fcb07f5e84eb0
     #print zip_ref.getinfo()
     _done = []
     _zipDir = None
-    for f in zip_ref.namelist():
-        if  'mayaTools' in f:
-            log.debug("Unzipping: {0}...".format(f))
-            _done.append(f)
-            if not _zipDir:
-                _zipDir = ''.join(f.split('mayaTools')[:-1])
-                _match = True
-                while _match:
-                    for c in ["/",_sep]:
-                        if _zipDir.endswith(c):
-                            _zipDir=''.join(list(_zipDir)[:-1])
-                    _match = False
-                    
-                    
-            zip_ref.extract(f,_dir)
+    _zipRoot = None
+    _name_list = zip_ref.namelist()
     
-    zip_ref.close()
-    
-    print _dir
-    print _zipDir
-
-    _path_mayaTools = _sep.join([_dir,_zipDir,'mayaTools'])
-    
-    for f in os.listdir(_path_mayaTools):
-        _dst = _sep.join([_dir,f])
-        _src = _sep.join([_path_mayaTools,f])
-        log.debug("Copy: {0} >> {1} ...".format(_src,_dst))
+    try:
+        mayaMainProgressBar = mel.eval('$tmp = $gMainProgressBar');
+        mc.progressBar( mayaMainProgressBar,
+                        edit=True,
+                        beginProgress=True,
+                        isInterruptable=True,
+                        status="Exctracting: {0}".format(_path),
+                        minValue = 0,
+                        step=1,
+                        vis=True,
+                        maxValue= len(_name_list))
         
-        try:move(_src, _dst)
-        except:pass
+        for i,f in enumerate(zip_ref.namelist()):
+            mc.progressBar(mayaMainProgressBar,
+                           edit=True,
+                           progress = i)
+            
+            if  'mayaTools' in f:
+                log.debug("Unzipping: {0}...".format(f))
+                _done.append(f)
+                if not _zipDir:
+                    _zipDir = ''.join(f.split('mayaTools')[:-1])
+                    _match = True
+                    while _match:
+                        for c in ["/",_sep]:
+                            if _zipDir.endswith(c):
+                                _zipDir=''.join(list(_zipDir)[:-1])
+                        _match = False
+                if not _zipRoot:
+                    _zipRoot = ''.join(f.split('mayaTools')[0])
+                    _match = True
+                    while _match:
+                        for c in ["/",_sep]:
+                            if _zipRoot.endswith(c):
+                                _zipRoot=''.join(list(_zipRoot)[:-1])
+                        _match = False
+                        
+                zip_ref.extract(f,_dir)
+        zip_ref.close()
+        
+        log.debug("Dir: {0}".format(_dir))
+        log.debug("Zip Dir: {0}".format(_zipDir))
+        log.debug("Zip Root: {0}".format(_zipRoot))
+        
+        if cleanFirst:
+            log.debug("Unzip: cleaning...")        
+            clean_install_path(_dir)    
+        
+        _path_mayaTools = _sep.join([_dir,_zipDir,'mayaTools'])
+        log.debug("Path mayaTools: {0}".format(_path_mayaTools))
+        
+        for f in os.listdir(_path_mayaTools):
+            _dst = _sep.join([_dir,f])
+            _src = _sep.join([_path_mayaTools,f])
+            log.debug("Copy: {0} >> {1} ...".format(_src,_dst))
+            
+            try:move(_src, _dst)
+            except:pass
+            
+        try:rmtree(_sep.join([_dir,_zipDir]))
+        except Exception,err:log.debug("Remove unzip temp fail | {0}".format(err))
+            
+        if deleteZip:
+            try:os.unlink(zFile)
+            except Exception,err:log.error("Delete zip fail | {0}".format(err))
+    except Exception, err:
+        log.error("Zip exception: {0}".format(err))
+    finally:
+        if mayaMainProgressBar:mc.progressBar(mayaMainProgressBar, edit=True, endProgress=True)
+        
+
+def download(url='http://weknowyourdreams.com/images/mountain/mountain-03.jpg', mode = None):
+    """
+    :Attribution
+    https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
     
-def download(url='http://weknowyourdreams.com/images/mountain/mountain-03.jpg'):
+    """
     log.debug(" url: {0}".format(url))
         
-        
-    #file = webbrowser.open(url, new=0, autoraise=True)    
-    #log.info(file)
+    if mode == 'default':
+        file = webbrowser.open(url, new=0, autoraise=True)    
+        log.info("Go check your default download folder for: {0}".format(''.join(url.split('/')[-1])))
+        return
     
     mayaMainProgressBar = None
     file_size_dl = 0
@@ -144,7 +222,7 @@ def download(url='http://weknowyourdreams.com/images/mountain/mountain-03.jpg'):
                             minValue = 0,
                             step=1,
                             vis=True,
-                            maxValue= 100 )            
+                            maxValue= 100 )
         
         while True:
             buffer = u.read(block_sz)
@@ -154,11 +232,15 @@ def download(url='http://weknowyourdreams.com/images/mountain/mountain-03.jpg'):
             file_size_dl += len(buffer)
             f.write(buffer)
             if file_size:
-                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-                #mc.progressBar(mayaMainProgressBar, edit=True, step = file_size_dl * 100. / file_size)                
-                #status = status + chr(8)*(len(status)+1)
-                log.debug( status )
-                
+                prcnt = file_size_dl * 100. / file_size
+                #status = r"%10d  [%3.2f%%]" % (file_size_dl, prcnt)
+                status = r"  [%3.2f%%]" % (prcnt,)
+                mc.progressBar(mayaMainProgressBar,
+                               edit=True,
+                               progress = (int(prcnt))-1,
+                               maxValue=100)
+                #status = status + chr(8)*( int(prcnt))
+                #print( status )
         f.close()
         return file_name
     except URLError, e:
@@ -169,7 +251,7 @@ def download(url='http://weknowyourdreams.com/images/mountain/mountain-03.jpg'):
         print '...'            
     
 
-def get_build(branch = 'mrs', idx = 0):
+def get_build(branch = _defaultBranch, idx = 0, mode = None):
     """
     
     """
@@ -188,6 +270,9 @@ def get_build(branch = 'mrs', idx = 0):
     #Get our zip
     url =  "https://bitbucket.org/jjburton/cgmtools/get/" + _dat['hash'] + ".zip"
     log.debug(" url: {0}".format(url))
+    
+    if mode == 'url':
+        return url
     
     #Download...
     _zip = download(url)
@@ -211,7 +296,7 @@ def get_dat(branch = 'master', limit = 3, update = False):
                 log.debug("passing buffer...")                          
                 return _dat
             
-    route = _pathMount + str(branch).lower()
+    route = _pathMount + branch
 
     request = Request(route)
     log.debug('Route: {0}'.format(request))
@@ -246,6 +331,65 @@ def get_dat(branch = 'master', limit = 3, update = False):
     finally:
         print '...'
         
+def here(branch = _defaultBranch, idx = 0):
+    """
+    """
+    _str_func = 'here'
+    _path = get_install_path()
+    log.debug("|{0}| >> path: {1}".format(_str_func,_path))
+    
+    _zip = get_build(branch,idx)
+    log.debug("|{0}| >> zip: {1}".format(_str_func,_zip))
+    
+    unzip(_zip,True,True)
+    
+    import cgm
+    cgm.core._reload()
 
+def ryan():
+    from urllib2 import Request, urlopen, URLError
+    import webbrowser
+    import json
+    
+    while 1==1:
+        branch = raw_input("Enter the name of the Branch ('Master' or 'Stable') to see a summary of last 10 commits: ")
+        mount = 'https://api.bitbucket.org/2.0/repositories/jjburton/cgmtools/commits/?until='
+        if (branch == 'master' or branch == 'Master' or branch== 'MASTER'):
+            route = mount+'master'
+            break
+        if (branch == 'stable' or branch == 'Stable' or branch == 'STABLE'):
+            route = mount+'stable'
+            break
+    request = Request(route)
+    
+    try:
+        response = urlopen(request)
+        stable = json.load(response)
+        idx = 1
+        last10 = []
+        print 'Here Are the Last 10 Commits:'
+    
+        while idx < 11:
+            hsh = stable['values'][idx]['hash']
+            msg = stable['values'][idx]['message']
+            last10.append({'hash':hsh, 'message':msg, 'index':idx})
+            print '#' + str(idx) + '. ' + 'https://bitbucket.org/jjburton/cgmtools/commits/' + hsh, ' msg: ' + msg
+            idx+=1
+        match = 'no'
+    
+        while match == 'no' :
+            commitIdx = int(raw_input("Enter Commit # (1-10) to download a download associated .zip files: "))
+            for commit in last10 :
+                if commit['index'] == commitIdx:
+                    match = 'yes'
+                    print 'Success! Downloading Zip Files for this commit: ' + 'https://bitbucket.org/' + commit['hash']
+                    url =  "https://bitbucket.org/jjburton/cgmtools/get/" + commit['hash'] + ".zip"
+                    file = webbrowser.open(url, new=0, autoraise=True)
+                if match == 'no' :
+                    print 'Please enter a number 1-10'
+    
+    except URLError, e:
+        print 'It appears this is not working...URL or Timeout Error :(', e
+        
 
 
