618d4761f4e187eceb5c618a122ab1d27d2b4724
Josh Burton <jjburton@gmail.com>
2019-01-01 21:08:09 -0600

    CURVES - loftSquare needed to be reversed BUILDER - change version

--- mayaTools/cgm/core/mrs/Builder.py
+++ mayaTools/cgm/core/mrs/Builder.py
@@ -69,7 +69,7 @@ import cgm.core.classes.GuiFactory as cgmUI
 mUI = cgmUI.mUI
 
 #>>> Root settings =============================================================
-__version__ = '1.10242018'
+__version__ = '1.12282018'
 _sidePadding = 25
 
 def check_cgm():
