f73e8458f97dfd6f29a6625db4b30db464ab7957
Josh Burton <jjburton@gmail.com>
2019-04-05 07:36:48 -0500

    red9 update

--- mayaTools/Red9/startup/language_packs/language_english.py
+++ mayaTools/Red9/startup/language_packs/language_english.py
@@ -39,8 +39,8 @@ class _MainMenus_(object):
 
     red9_homepage = "Red9_HomePage"
     red9_homepage_ann = "Open Red9Consultancy HomePage"
-    red9_blog = "Red9_Blog"
-    red9_blog_ann = "Open Red9Blog"
+    red9_blog = "Red9_News : Latest Tools!"
+    red9_blog_ann = "Open Red9 News Feed"
     red9_vimeo = "Red9_Vimeo Channel"
     red9_vimeo_ann = "Open Red9Vimeo Channel"
     red9_facebook = "Red9_Facebook"
@@ -72,6 +72,8 @@ class _MainMenus_(object):
     open_in_explorer_ann = "Open the folder containing the current Maya Scene"
     copy_to_clipboard = 'Red9: Copy to Clipboard'
     copy_to_clipboard_ann = 'Copy the current Maya filepath to the OS:Clipboard'
+    open_r9anim = 'Red9 PRO: Import r9Anim Direct'
+    open_r9anim_ann = 'Import an r9Anim file direct IF it has internal reference file pointers (mRig Only)'
 
     # TimeSlider menu additions
     collapse_time = 'Red9: Collapse Time'
