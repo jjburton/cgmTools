c021bf14814e1026b6f9ea2192cbfb0f8fc65536
Josh Burton <jjburton@gmail.com>
2021-01-05 21:01:58 -0600

    GEN | release string was wrong

--- mayaTools/cgm/core/cgm_General.py
+++ mayaTools/cgm/core/cgm_General.py
@@ -10,7 +10,7 @@ For help on cgmFuncCls - cgm.core.examples.help_cgmFuncCls
 ================================================================
 """
 __MAYALOCAL = 'cgmGEN'
-__RELEASE = '21.4.2.1'
+__RELEASE = '21.4.0.3'
 __BRANCH = 'MRSDEV'
 
 import maya.cmds as mc
