58a3d71bc5ffef050d94eaedb4a361c80f14ae0d
Josh Burton <jjburton@gmail.com>
2021-01-10 22:05:25 -0600

    cgmGEN | version string fix

--- mayaTools/cgm/core/cgm_General.py
+++ mayaTools/cgm/core/cgm_General.py
@@ -10,7 +10,7 @@ For help on cgmFuncCls - cgm.core.examples.help_cgmFuncCls
 ================================================================
 """
 __MAYALOCAL = 'cgmGEN'
-__RELEASE = '21.4.0.3'
+__RELEASE = '21.4.0.4'
 __BRANCH = 'MRSDEV'
 
 import maya.cmds as mc
