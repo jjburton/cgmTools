839680d707271ad8293e9483a4f79920751b1077
brazosimos\jjburton <jjburton@gmail.com>
2021-08-16 09:21:28 -0500

    CGMGEN | release strings

--- mayaTools/cgm/core/cgm_General.py
+++ mayaTools/cgm/core/cgm_General.py
@@ -11,9 +11,9 @@ For help on cgmFuncCls - cgm.core.examples.help_cgmFuncCls
 """
 __MAYALOCAL = 'cgmGEN'
 
-__RELEASE = '21.8.15.1'
+__RELEASE = '21.8.16.1'
 
-__BRANCH = 'MRSDEV'
+__BRANCH = 'MRSDAILY'
 
 import maya.cmds as mc
 import maya.mel as mel
